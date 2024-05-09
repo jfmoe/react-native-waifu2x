9181edb73cd0c14444baa32c61392c82b7c921d4
jfmoe <34602628+jfmoe@users.noreply.github.com>
2024-05-09 23:55:24 +0800

    build: delete android files

--- android/src/main/java/expo/modules/waifu2x/ReactNativeWaifu2xView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.waifu2x
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ReactNativeWaifu2xView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
